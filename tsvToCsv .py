511652a75c7bc7212dfc9d52b14f9ce8dfd11000
Yajat Kumar <yajatapps3@gmail.com>
2022-02-26 20:30:03 -0800

    Add tsv to csv converter

---
+++ tsvToCsv .py
@@ -0,0 +1,20 @@
+# Attempt to convert tsv to csv compatible with scvis by using pandas package
+
+# Source: https://www.geeksforgeeks.org/python-convert-tsv-to-csv-file/
+# https://stackoverflow.com/questions/59970956/delete-first-rows-of-csv-file-in-python
+
+import pandas as pd 
+  
+tsv_file='bipolar_pca100.tsv'
+  
+# readinag given tsv file
+csv_table=pd.read_table(tsv_file,sep='\t')
+  
+# converting tsv file into csv
+csv_table.to_csv('output.csv',index=False)
+  
+df = pd.read_csv('output.csv', skiprows=1)
+df.to_csv('out.csv', index=False)  
+
+# output
+print("Successfully made csv file")
