3b7ece2a42ebd8a105dce2fdf145560a5177b4dc
YAJATapps <yajatapps3@gmail.com>
2022-03-27 18:15:41 -0700

    Add k means plotter

---
+++ kMeansPlot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.cluster import KMeans
+
+dataset= pd.read_csv('test.csv')
+X=dataset.iloc[:, [0,1]].values
+
+kmeans = KMeans(n_clusters=26)
+kmeans.fit(X)
+y_kmeans = kmeans.predict(X)
+
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans)
+plt.show()
+
